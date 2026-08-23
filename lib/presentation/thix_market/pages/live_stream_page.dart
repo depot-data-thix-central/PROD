dce6ea9dfbe9731e435fc1a6c890ec2d4c50a50e
depot-data-thix-central <datathix@gmail.com>
2026-08-23 15:34:09 +0300

    Update live_stream_page.dart

--- lib/presentation/thix_market/pages/live_stream_page.dart
+++ lib/presentation/thix_market/pages/live_stream_page.dart
@@ -1,12 +1,20 @@
 // lib/presentation/thix_market/pages/live_stream_page.dart
 import 'package:flutter/material.dart';
-
 import '../widgets/live/live_stream_player.dart';
 
 class LiveStreamPage extends StatelessWidget {
   final String liveId;
+  final String? token;
+  final String? channelName;
+  final bool isHost; // Permet de savoir si c'est le diffuseur ou un spectateur
 
-  const LiveStreamPage({super.key, required this.liveId});
+  const LiveStreamPage({
+    super.key, 
+    required this.liveId,
+    this.token,
+    this.channelName,
+    this.isHost = false, // Spectateur par défaut
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -21,6 +29,13 @@ class LiveStreamPage extends StatelessWidget {
         ),
       );
     }
-    return LiveStreamPlayer(channelName: liveId, liveId: liveId);
+    
+    // On passe TOUTES les informations au vrai lecteur vidéo
+    return LiveStreamPlayer(
+      liveId: liveId,
+      channelName: channelName ?? liveId, 
+      token: token,
+      isHost: isHost, // C'est ici que la magie s'opère pour allumer ta caméra !
+    );
   }
 }
