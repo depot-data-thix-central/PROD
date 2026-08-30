9f9714186921ed2ed6cdc71710e9f42922047daa
depot-data-thix-central <datathix@gmail.com>
2026-08-30 17:06:02 +0300

    Update shop_statistics_page.dart

--- lib/presentation/thix_market/pages/shop_statistics_page.dart
+++ lib/presentation/thix_market/pages/shop_statistics_page.dart
@@ -5,7 +5,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:html/parser.dart' as html_parser;
-
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:thix_id/core/theme/thix_design_policy.dart';
 import '../providers/market_providers.dart';
 
