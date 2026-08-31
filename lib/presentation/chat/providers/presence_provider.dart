ada465efb6c34d2a1ae73cab9f853afdc2674ad8
depot-data-thix-central <datathix@gmail.com>
2026-08-31 21:33:52 +0300

    Update presence_provider.dart

--- lib/presentation/chat/providers/presence_provider.dart
+++ lib/presentation/chat/providers/presence_provider.dart
@@ -4,7 +4,7 @@ import 'dart:async';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-
+import 'package:thix_id/features/auth/presentation/providers/auth_controller.dart';
 import 'package:thix_id/auth/auth_controller.dart';
 import 'package:thix_id/models/app_user.dart';
 
