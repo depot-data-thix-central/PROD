191d1e78b761b31e1864c47fc9a1159dc91dd2b7
depot-data-thix-central <datathix@gmail.com>
2026-08-31 10:01:29 +0300

    Update chat_screen.dart

--- lib/presentation/chat/chat_screen.dart
+++ lib/presentation/chat/chat_screen.dart
@@ -2505,7 +2505,7 @@ class _ThixChatBackgroundPainter extends CustomPainter {
     );
     final textPainter = TextPainter(
       text: TextSpan(text: 'THIX CHAT', style: textStyle),
-      textDirection: TextDirection.ltr,
+      textDirection: ui.TextDirection.ltr,
     );
     textPainter.layout();
 
