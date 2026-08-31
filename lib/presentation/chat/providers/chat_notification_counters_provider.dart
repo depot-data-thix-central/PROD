75b2e5ad91a0508be6f4879f9baad1e51b3980fc
depot-data-thix-central <datathix@gmail.com>
2026-08-31 20:13:07 +0300

    Update chat_notification_counters_provider.dart

--- lib/presentation/chat/providers/chat_notification_counters_provider.dart
+++ lib/presentation/chat/providers/chat_notification_counters_provider.dart
@@ -23,10 +23,13 @@ import 'dart:async';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-
 import 'package:thix_id/auth/auth_controller.dart' show currentUserProvider;
 import 'package:thix_id/presentation/chat/providers/chat_providers.dart'
     show supabaseClientProvider;
+import 'package:thix_id/models/app_user.dart';
+import 'package:thix_id/features/auth/presentation/providers/auth_controller.dart';
+
+
 
 // ============================================================================
 // CONSTANTS
