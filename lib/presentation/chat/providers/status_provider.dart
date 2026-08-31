16e7a590384f7775da229df8cf7eea8b57f328f6
depot-data-thix-central <datathix@gmail.com>
2026-08-31 21:56:23 +0300

    Update status_provider.dart

--- lib/presentation/chat/providers/status_provider.dart
+++ lib/presentation/chat/providers/status_provider.dart
@@ -7,7 +7,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:thix_id/features/auth/presentation/providers/auth_controller.dart'; // 👈 C'est lui qui définit currentUserProvider
 import 'package:thix_id/models/chat/user_status_story.dart';
 import 'package:thix_id/presentation/chat/providers/chat_providers.dart';
-
+import 'package:thix_id/auth/auth_controller.dart' show currentUserProvider;
+// (change 'features/auth/presentation/providers/auth_controller.dart' → 'auth/auth_controller.dart')
 
 // ============================================================================
 // CONSTANTS
