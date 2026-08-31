6269de30556655bb29f085d38611ccfab77444dc
depot-data-thix-central <datathix@gmail.com>
2026-08-31 12:40:42 +0300

    Update chat_settings_page.dart

--- lib/presentation/chat/settings/chat_settings_page.dart
+++ lib/presentation/chat/settings/chat_settings_page.dart
@@ -6,7 +6,7 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'package:thix_id/core/theme/thix_design_policy.dart';
 import 'package:thix_id/l10n/app_localizations.dart';
-import 'package:thix_id/presentation/auth/providers/auth_controller.dart'; // Assurez-vous que ce provider existe
+import 'package:thix_id/auth/auth_controller.dart';
 import 'package:thix_id/presentation/chat/profile/chat_profile_page.dart';
 import 'package:thix_id/providers/chat/chat_settings_provider.dart';
 import 'package:thix_id/presentation/chat/settings/chat_appearance_settings.dart';
