57b333f4da969a0efd030dbe680f0d19765d8027
depot-data-thix-central <datathix@gmail.com>
2026-09-02 10:12:08 +0300

    Update admin_news_page.dart

--- lib/presentation/admin/pages/admin_news_page.dart
+++ lib/presentation/admin/pages/admin_news_page.dart
@@ -1,11 +1,12 @@
 // lib/presentation/admin/pages/admin_news_page.dart
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 
-import '../../../providers/news_provider.dart';
-import '../../../services/news_service.dart';
-import 'admin_news_dashboard.dart';  // ← Chemin corrigé (même dossier)
+// Décommenter si nécessaire dans le futur
+// import 'package:provider/provider.dart';
+// import 'package:supabase_flutter/supabase_flutter.dart';
+// import '../../../providers/news_provider.dart';
+// import '../../../services/news_service.dart';
+import 'admin_news_dashboard.dart';
 
 class AdminNewsPage extends StatelessWidget {
   final String role;
@@ -14,9 +15,6 @@ class AdminNewsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => NewsProvider(NewsService(Supabase.instance.client)),
-      child: const AdminNewsDashboard(),
-    );
+    return const AdminNewsDashboard();
   }
 }
