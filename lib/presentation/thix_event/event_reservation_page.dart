fcff3805cd165b2741f67b0f0da89d3e0a14a33e
depot-data-thix-central <datathix@gmail.com>
2026-09-06 09:34:12 +0300

    Update event_reservation_page.dart

--- lib/presentation/thix_event/event_reservation_page.dart
+++ lib/presentation/thix_event/event_reservation_page.dart
@@ -17,7 +17,7 @@
 import 'dart:async';
 import 'dart:math';
 import 'dart:ui';
-
+import '../../providers/event_provider.dart'; 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
