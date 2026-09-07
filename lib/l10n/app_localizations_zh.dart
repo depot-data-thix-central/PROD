8baf7bb0f724aa18b7466d2936fd47a5acc3dc34
depot-data-thix-central <datathix@gmail.com>
2026-09-07 20:04:14 +0300

    Update app_localizations_zh.dart

--- lib/l10n/app_localizations_zh.dart
+++ lib/l10n/app_localizations_zh.dart
@@ -1,5 +1,6 @@
 // lib/l10n/app_localizations_zh.dart
 import 'app_localizations.dart';
+  @override Locale get locale => const Locale('zh');
 
 class AppLocalizationsZh extends AppLocalizations {
   // ============================================================================
